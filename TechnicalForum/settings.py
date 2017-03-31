8f7c5c2d0a2cc7fd4a95d14db6d8653cc720621e
vamsikrish53 <vamsikrish.49@gmail.com>
2017-03-31 11:01:59 +0530

    Update settings.py

--- TechnicalForum/settings.py
+++ TechnicalForum/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'home/TechForums/TechnicalForum/Forum/static/CSS'
+STATIC_ROOT = 'home/TechForums/TechnicalForum/Forum/static/css'
